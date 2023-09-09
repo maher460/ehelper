2ff4b1cd6a0d4d48c54d827eb8b04f574298829d
Sabit Hassan <sabithassan64@gmail.com>
2023-09-09 19:36:17 -0400

    testing change

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "<center><h1>Flask App on Azure by Maher</h1></center>"
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
